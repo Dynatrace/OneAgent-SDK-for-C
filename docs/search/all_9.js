9c858786d78091ae67cd104c4e3d3d02723862f7
Christian Neumüller <christian.neumueller@dynatrace.com>
2019-02-07 13:00:17 +0100

    Update to version 1.3.2.
    * Solaris SPARC support.
    * Minor reference documentation improvements.

---
+++ docs/search/all_9.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['version_5fmajor',['version_major',['../structonesdk__stub__version__t.html#ad7e4c05bc2e33cb9fbfd153a3ab4279a',1,'onesdk_stub_version_t']]],
+  ['version_5fminor',['version_minor',['../structonesdk__stub__version__t.html#a03bee0ac0f108ba112a6ca88f99a87eb',1,'onesdk_stub_version_t']]],
+  ['version_5fpatch',['version_patch',['../structonesdk__stub__version__t.html#afefa0d14ecdeb503b50d563fb6b3ca32',1,'onesdk_stub_version_t']]]
+];
